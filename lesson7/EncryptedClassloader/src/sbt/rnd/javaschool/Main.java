7f42d864ba86db2473ad08c8a3f77d4535555e6a
pavelgov <webdondesign@yandex.ru>
2017-09-17 12:24:38 +0300

    Revert "lesson7"
    This reverts commit a6c073f3fa0633f85930b1a4968a1afd5595568d.

--- lesson7/EncryptedClassloader/src/sbt/rnd/javaschool/Main.java
+++ lesson7/EncryptedClassloader/src/sbt/rnd/javaschool/Main.java
@@ -1,21 +1,8 @@
 package sbt.rnd.javaschool;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
-
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, IOException {
-
-      /*
-      // шифруем файл
-        new Decryptor().decryptFile(Paths.get("C:\\Sbt\\Sbt\\lesson7\\EncryptedClassloader\\src\\sbt\\rnd\\javaschool\\plugins\\Plg.class"),"key".getBytes());
-      */
-
-        EncryptedClassloader encLoader = new EncryptedClassloader("key", new File("C:\\Sbt\\Sbt\\lesson7\\EncryptedClassloader\\src\\sbt\\rnd\\javaschool\\plugins"), Main.class.getClassLoader());
-        Class<?> claz = encLoader.loadClass("sbt.rnd.javaschool.plugins.Plg");
-        Plugin plugin = (Plugin) claz.newInstance();
-        plugin.doUsefull();
+    public static void main(String[] args) {
+	// write your code here
     }
 }
--- lesson7/EncryptedClassloader/src/sbt/rnd/javaschool/Plugin.java
+++
@@ -1,8 +0,0 @@
-package sbt.rnd.javaschool;
-
-/**
- * Created by User on 13.09.2017.
- */
-public interface Plugin {
-    void doUsefull();
-}
